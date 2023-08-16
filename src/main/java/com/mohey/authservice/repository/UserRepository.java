96494feb3e9ff2f593583f5d81e39d1e6e2a58ca
박현철 <wns882@naver.com>
2023-08-16 10:26:23 +0900

    :bug: fix(Login) 회원 탈퇴 사용자 로그인 불가 추가
    회원 탈퇴 사용자 로그인 불가 추가

--- src/main/java/com/mohey/authservice/repository/UserRepository.java
+++ src/main/java/com/mohey/authservice/repository/UserRepository.java
@@ -1,19 +1,23 @@
 package com.mohey.authservice.repository;
 
-import com.mohey.authservice.domain.User;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import com.mohey.authservice.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // select * from user where username = ? 이렇게 동작
+	// select * from user where username = ? 이렇게 동작
 
-    Optional<User> findByKakaoId(String kakaoId); // Jpa NameQuery 작동
-    //Optional<User> findByUsername(String username);
-    //null일 수도 있어서 감싸주는거 null 방어코드 짜줘야함
-    User findByMemberUuid(String memberUuid);
+	@Query("SELECT u FROM User u LEFT JOIN UserWithdrawal uw ON u.id = uw.userId.id WHERE u.kakaoId = :kakaoId AND (uw.status IS NULL OR uw.status = true) AND u.createdDatetime = (SELECT MAX(u2.createdDatetime) FROM User u2 WHERE u2.kakaoId = :kakaoId)")
+	Optional<User> findByKakaoId(String kakaoId);
 
-    // save - 이미 만들어져 있음. jparepo가 이미 만들어 줌 테스트 필요 없다
+	// Jpa NameQuery 작동
+	//Optional<User> findByUsername(String username);
+	//null일 수도 있어서 감싸주는거 null 방어코드 짜줘야함
+	User findByMemberUuid(String memberUuid);
+
+	// save - 이미 만들어져 있음. jparepo가 이미 만들어 줌 테스트 필요 없다
 }
--- src/main/java/com/mohey/authservice/service/UserServiceImpl.java
+++ src/main/java/com/mohey/authservice/service/UserServiceImpl.java
@@ -1,165 +1,165 @@
 package com.mohey.authservice.service;
 
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.mohey.authservice.config.auth.LoginUser;
-import com.mohey.authservice.config.jwt.JwtVO;
-import com.mohey.authservice.domain.UserDeviceInfo;
-import com.mohey.authservice.domain.UserEnum;
-import com.mohey.authservice.domain.UserWithdrawal;
-import com.mohey.authservice.dto.*;
-import com.mohey.authservice.repository.UserDeviceInfoRepository;
-import com.mohey.authservice.repository.UserRepository;
-import com.mohey.authservice.repository.UserWithdrawalRepository;
-import com.mohey.authservice.service.client.MemberServiceClient;
+import java.rmi.ServerException;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.mohey.authservice.config.jwt.JwtVO;
 import com.mohey.authservice.domain.User;
+import com.mohey.authservice.domain.UserDeviceInfo;
+import com.mohey.authservice.domain.UserEnum;
+import com.mohey.authservice.domain.UserWithdrawal;
+import com.mohey.authservice.dto.DeleteReqDto;
+import com.mohey.authservice.dto.DeviceUuidRespDto;
+import com.mohey.authservice.dto.JoinReqDto;
+import com.mohey.authservice.dto.JoinRespDto;
+import com.mohey.authservice.dto.JoinRespToMembersDto;
+import com.mohey.authservice.repository.UserDeviceInfoRepository;
+import com.mohey.authservice.repository.UserRepository;
+import com.mohey.authservice.repository.UserWithdrawalRepository;
+import com.mohey.authservice.service.client.MemberServiceClient;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.web.client.HttpServerErrorException;
-
-import java.rmi.ServerException;
-import java.time.LocalDateTime;
-import java.util.Optional;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
 @Slf4j
 public class UserServiceImpl implements UserService {
-    private final RedisService redisService;
-    private final ImageService imageService;
+	private final RedisService redisService;
+	private final ImageService imageService;
 
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	private final UserRepository userRepository;
+	private final UserWithdrawalRepository userWithdrawalRepository;
+	private final KaKaoLoginService kakaoLoginService;
+	private final MemberServiceClient memberServiceClient;
+	private final UserDeviceInfoRepository userDeviceInfoRepository;
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
-    private final UserRepository userRepository;
-    private final UserWithdrawalRepository userWithdrawalRepository;
-    private final KaKaoLoginService kakaoLoginService;
-    private final MemberServiceClient memberServiceClient;
-    private final UserDeviceInfoRepository userDeviceInfoRepository;
+	// 서비스는 DTO를 요청받고, DTO로 응답한다.
+	@Transactional // 트랜잭션이 메서드 시작할 때, 시작되고, 종료될 때 함께 종료
+	public JoinRespDto join(JoinReqDto joinReqDto) throws ServerException {
 
+		String imageUrl = "";
+		//S3에 이미지 url 가지고 오기
+		if (!joinReqDto.getProfileUrl().equals("")) {
+			imageUrl = imageService.getImageUrl(joinReqDto.getProfileUrl());
+		}
+		log.info(imageUrl);
 
-    // 서비스는 DTO를 요청받고, DTO로 응답한다.
-    @Transactional // 트랜잭션이 메서드 시작할 때, 시작되고, 종료될 때 함께 종료
-    public JoinRespDto join(JoinReqDto joinReqDto) throws ServerException {
+		//db있는지 확인!
+		String kakaoId = kakaoLoginService.createKakaoUser(joinReqDto.getAccessToken());
+		//System.out.println(userRepository.findByUsername(kakaoId));
+		//.isPresent() empty면 false 값이 있으면 true
+		if (userRepository.findByKakaoId(kakaoId).isPresent()) {
+			//이미 있는 사용자니까 로그인하라고 하기
+			throw new ServerException("이미 가입된 사용자입니다.");
 
-        String imageUrl="";
-        //S3에 이미지 url 가지고 오기
-        if (!joinReqDto.getProfileUrl().equals("")){
-            imageUrl = imageService.getImageUrl(joinReqDto.getProfileUrl());
-        }
-        log.info(imageUrl);
+		}
+		String memberUuid = UUID.randomUUID().toString();
+		String deviceUuid = UUID.randomUUID().toString();
 
-        //db있는지 확인!
-        String kakaoId = kakaoLoginService.createKakaoUser(joinReqDto.getAccessToken());
-        //System.out.println(userRepository.findByUsername(kakaoId));
-        //.isPresent() empty면 false 값이 있으면 true
-        if(userRepository.findByKakaoId(kakaoId).isPresent()){
-            //이미 있는 사용자니까 로그인하라고 하기
-            throw new ServerException("이미 가입된 사용자입니다.");
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String password = passwordEncoder.encode("");
+		System.out.println("deviceUuid: " + deviceUuid);
 
-        }
-        String memberUuid = UUID.randomUUID().toString();
-        String deviceUuid = UUID.randomUUID().toString();
+		//액세스토큰으로 카카오 아이디 받아와서 객체 만드는 걸로 변경
+		userRepository.save(new User(kakaoId, memberUuid, password, UserEnum.MEMBER, LocalDateTime.now()));
 
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String password = passwordEncoder.encode("");
-        System.out.println("deviceUuid: "+deviceUuid);
+		//여기서 멤버한테 보내야 함. .. .!!
+		//dto만들어주고
+		//client에 있는 메서드 불러오기
+		//System.out.println("joinfeign1");
+		memberServiceClient.join(
+			new JoinRespToMembersDto(memberUuid, joinReqDto.getBirthDate(), joinReqDto.getGender(), deviceUuid,
+				joinReqDto.getDeviceToken(), joinReqDto.getSelfIntroduction(), imageUrl, joinReqDto.getInterests(),
+				joinReqDto.getNickname()));
+		//System.out.println("joinfeign2");
 
-        //액세스토큰으로 카카오 아이디 받아와서 객체 만드는 걸로 변경
-        userRepository.save(new User(kakaoId, memberUuid,password, UserEnum.MEMBER, LocalDateTime.now()));
+		// 3. dto 응답
+		//잘 된 경우
+		//        if (memberJoinSuccess) {
+		//            return new JoinRespDto(userPS);
+		//        }
 
+		//System.out.println("멤버 정보 넘겨주기 실패 !");
+		return new JoinRespDto(memberUuid);
+	}
 
-        //여기서 멤버한테 보내야 함. .. .!!
-        //dto만들어주고
-        //client에 있는 메서드 불러오기
-        //System.out.println("joinfeign1");
-        memberServiceClient.join(new JoinRespToMembersDto(memberUuid,joinReqDto.getBirthDate(),joinReqDto.getGender(),deviceUuid,joinReqDto.getDeviceToken(),joinReqDto.getSelfIntroduction(), imageUrl, joinReqDto.getInterests(), joinReqDto.getNickname()));
-        //System.out.println("joinfeign2");
+	//로그아웃, 회원탈퇴
 
-        // 3. dto 응답
-        //잘 된 경우
-//        if (memberJoinSuccess) {
-//            return new JoinRespDto(userPS);
-//        }
+	public void withDraw(DeleteReqDto deleteReqDto) {
 
-        //System.out.println("멤버 정보 넘겨주기 실패 !");
-        return new JoinRespDto(memberUuid);
-    }
+		//회원 탈퇴 상태만 바꿔서 다시 포스트로 쏘기
+		User user = userRepository.findByMemberUuid(deleteReqDto.getMemberUuid());
+		//status 변경해서 userWithdrawal 만들어주기
 
-    //로그아웃, 회원탈퇴
+		UserWithdrawal userWithdrawal = UserWithdrawal.builder()
+			.userId(user)
+			.status(false)
+			.build();
+		userWithdrawalRepository.save(userWithdrawal);
+		//리턴 필요 x
+	}
 
-    public void withDraw(DeleteReqDto deleteReqDto) {
+	public void logout(String memberUuid) {
+		//리프레시토큰을 만료시키기
+		redisService.expireValues(memberUuid);
+	}
 
-        //회원 탈퇴 상태만 바꿔서 다시 포스트로 쏘기
-        User user = userRepository.findByMemberUuid(deleteReqDto.getMemberUuid());
-        //status 변경해서 userWithdrawal 만들어주기
+	@Override
+	public Boolean verifyRefreshToken(String refreshToken) {
+		try {
+			//PREFIX는 인코딩 안된거라 빼줘야됨
+			String realRefresh = refreshToken.substring(JwtVO.TOKEN_PREFIX.length());
+			//System.out.println(realRefresh);
+			DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(JwtVO.SECRET)).build().verify(realRefresh);
+			//System.out.println(decodedJWT);
+			String memberUuid = decodedJWT.getClaim("memberUuid").asString(); //토큰 내부는 암호화 안되어 있으니까 최소한의 정보만 넣자
+			String deviceUuid = decodedJWT.getClaim("deviceUuid").asString();
+			//System.out.println(memberUuid);
+			//System.out.println(deviceUuid);
+			//redisService.getValuse(deviceUuid+':'+memberUuid)로 찾아야 나옴... 이걸 어떻게 가져오지
+			//redisService.getValues(memberUuid)
+			//db에 저장된 거랑 같아 ??
+			if (redisService.getValues(deviceUuid + ':' + memberUuid) == null) {
+				return false;
+			} else {
+				return true;
+				//IllegalArgumentException, HttpServerErrorException.InternalServerError
+			}
+		} catch (Exception e) {
+			return false;
+		}
 
-        UserWithdrawal userWithdrawal = UserWithdrawal.builder()
-                .userId(user)
-                .build();
-        userWithdrawalRepository.save(userWithdrawal);
-        //리턴 필요 x
-    }
+	}
 
-    public void logout(String memberUuid){
-        //리프레시토큰을 만료시키기
-        redisService.expireValues(memberUuid);
-    }
+	@Override
+	public void verifyDeviceUuid(String deviceUuid, String deviceToken, String memberUuid) {
+		UserDeviceInfo userDeviceInfo = userDeviceInfoRepository.findByDeviceUuid(deviceUuid);
+		System.out.println("verifyDeviceUuid :" + userDeviceInfo);
+		if (userDeviceInfo == null) {
+			User user = userRepository.findByMemberUuid(memberUuid);
+			UserDeviceInfo userDeviceInfo1 = new UserDeviceInfo(user, deviceUuid);
+			userDeviceInfoRepository.save(userDeviceInfo1);
 
-    @Override
-    public Boolean verifyRefreshToken(String refreshToken) {
-        try{
-            //PREFIX는 인코딩 안된거라 빼줘야됨
-            String realRefresh = refreshToken.substring(JwtVO.TOKEN_PREFIX.length());
-            //System.out.println(realRefresh);
-            DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(JwtVO.SECRET)).build().verify(realRefresh);
-            //System.out.println(decodedJWT);
-            String memberUuid = decodedJWT.getClaim("memberUuid").asString(); //토큰 내부는 암호화 안되어 있으니까 최소한의 정보만 넣자
-            String deviceUuid = decodedJWT.getClaim("deviceUuid").asString();
-            //System.out.println(memberUuid);
-            //System.out.println(deviceUuid);
-            //redisService.getValuse(deviceUuid+':'+memberUuid)로 찾아야 나옴... 이걸 어떻게 가져오지
-            //redisService.getValues(memberUuid)
-            //db에 저장된 거랑 같아 ??
-            if (redisService.getValues(deviceUuid+':'+memberUuid) == null) {
-                return false;
-            } else {
-                return true;
-                //IllegalArgumentException, HttpServerErrorException.InternalServerError
-            }} catch (Exception e){
-            return false;
-        }
+			DeviceUuidRespDto deviceUuidRespDto = new DeviceUuidRespDto(memberUuid, deviceUuid, deviceToken);
+			//fein 처리
+			//System.out.println("ljlkjlkljkljk");
+			memberServiceClient.register(deviceUuidRespDto);
+			//System.out.println("qwerqwer");
+			//System.out.println("feign: "+memberServiceClient.register(deviceUuidRespDto));
 
-    }
-
-    @Override
-    public void verifyDeviceUuid(String deviceUuid,String deviceToken, String memberUuid) {
-        UserDeviceInfo userDeviceInfo = userDeviceInfoRepository.findByDeviceUuid(deviceUuid);
-        System.out.println("verifyDeviceUuid :"+ userDeviceInfo);
-        if(userDeviceInfo == null){
-            User user = userRepository.findByMemberUuid(memberUuid);
-            UserDeviceInfo userDeviceInfo1 = new UserDeviceInfo(user,deviceUuid);
-            userDeviceInfoRepository.save(userDeviceInfo1);
-
-            DeviceUuidRespDto deviceUuidRespDto = new DeviceUuidRespDto(memberUuid, deviceUuid, deviceToken);
-            //fein 처리
-            //System.out.println("ljlkjlkljkljk");
-            memberServiceClient.register(deviceUuidRespDto);
-            //System.out.println("qwerqwer");
-            //System.out.println("feign: "+memberServiceClient.register(deviceUuidRespDto));
-
-            //status에 따라 재요청
-        }}
+			//status에 따라 재요청
+		}
+	}
 }
